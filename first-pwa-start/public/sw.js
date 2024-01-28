bf6544e2089cf34b8e8660921e039d1cecce62f2
TranTam <tamvaa1306@gmail.com>
2024-01-28 20:04:57 +0700

    3.40

--- first-pwa-start/public/sw.js
+++ first-pwa-start/public/sw.js
@@ -1,7 +1,12 @@
 self.addEventListener("install", (event) => {
-  console.log(event);
+  console.log("Install event", event);
 });
 self.addEventListener("active", (event) => {
-  console.log(event);
+  console.log("Active event", event);
   return self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Fetching event", event);
+  event.repsondWith(fetch(event.request));
 });
